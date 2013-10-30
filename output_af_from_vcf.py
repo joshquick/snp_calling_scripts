a74679a9c0b2930d16d50d987bb3ff9a36fd3715
joshquick <j.quick@bham.ac.uk>
2013-10-30 11:24:56 +0000

    First commit of a script to generate a table of allele frequencies from a .vcf file.

---
+++ output_af_from_vcf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# Josh Quick 2013
+
+import vcf
+import sys
+import csv
+
+def main():
+	allele_freq = []
+	vcf_reader = vcf.Reader(sys.stdin)
+	records = [record for record in vcf_reader]
+	positions = [record.POS for record in records]
+	for i, record in enumerate(records):
+		samples = record.samples
+		if i == 0:
+			allele_freq.append([sample.sample for sample in samples])
+		freqs = [sample['FREQ'].split('%')[0] if not sample['FREQ'] == None else '' for sample in samples]
+		allele_freq.append(freqs)
+
+	with open("perc_count.txt", "wb") as fh_out:
+		writer = csv.writer(fh_out)
+		positions.insert(0, 'sample')
+		writer.writerow(positions)
+		writer.writerows(zip(*allele_freq))
+
+if __name__ == '__main__':
+	main()
